42a206c5b56b687f5fa4f0b60150b0a5e55824ef
איתן קטייב <etanss9911@gmail.com>
2024-03-21 18:20:49 +0200

    Changed to application from console

--- 8BitBounce/8BitBounce.cpp
+++ 8BitBounce/8BitBounce.cpp
@@ -5,7 +5,7 @@
 #include "BallWindow.h"
 using namespace std;
 
-int main()
+int WINAPI WinMain(HINSTANCE hInstance, HINSTANCE hPrevInstance, LPSTR lpCmdLine, int nCmdShow) 
 {
     Window* pWindow = new Window();
 
