1246810009a35c7b211edfde8c1d467b0713e68a
Snawyyy <76952482+Snawyyy@users.noreply.github.com>
2024-06-25 13:42:08 +0300

    seperated WindowPhysics to header and cpp files

---
+++ 8BitBounce/WindowPhysics.cpp
@@ -0,0 +1,31 @@
+#include "WindowPhysics.h"
+
+WindowPhysics::WindowPhysics(HWND WindowHandle) : RigidBody(WindowHandle)
+{
+    timerID = 0;
+    hasGravity = true; // Default to having gravity
+}
+
+void WindowPhysics::SetTimerID(UINT id)
+{
+    timerID = id;
+}
+
+UINT WindowPhysics::GetTimerID() const
+{
+    return timerID;
+}
+
+void WindowPhysics::UpdateSize()
+{
+    RECT windowRect;
+    GetWindowRect(hWnd, &windowRect);
+    width = windowRect.right - windowRect.left;
+    height = windowRect.bottom - windowRect.top;
+}
+
+void WindowPhysics::RunPhysics()
+{
+    RigidBody::RunPhysics();
+    SetWindowPos(hWnd, NULL, bodyX, bodyY, 0, 0, SWP_NOSIZE | SWP_NOZORDER);
+}
--- 8BitBounce/WindowPhysics.h
+++ 8BitBounce/WindowPhysics.h
@@ -4,31 +4,17 @@
 class WindowPhysics : public RigidBody
 {
 public:
+    bool hasGravity;
+    int id;
 
-	bool hasGravity;
+    WindowPhysics(HWND WindowHandle);
 
-	WindowPhysics(HWND WindowHandle) :RigidBody(WindowHandle)
-	{
-		timerID = 0;
-	}
+    void SetTimerID(UINT id);
+    UINT GetTimerID() const;
 
-	void SetTimerID(UINT id) { timerID = id; }
-	UINT GetTimerID() const { return timerID; }
-
-	void UpdateSize() override
-	{
-		RECT windowRect;
-		GetWindowRect(hWnd, &windowRect);
-		width = windowRect.right - windowRect.left;
-		height = windowRect.bottom - windowRect.top;
-	}
-
-	void RunPhysics() override
-	{
-		RigidBody::RunPhysics();
-		SetWindowPos(hWnd, NULL, bodyX, bodyY, 0, 0, SWP_NOSIZE | SWP_NOZORDER);
-	}
+    void UpdateSize() override;
+    void RunPhysics() override;
 
 private:
-	UINT timerID;
+    UINT timerID;
 };
