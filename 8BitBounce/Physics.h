40d18f717486b3ad2a4dd83b8a84caba70db4506
Snawyyy <76952482+Snawyyy@users.noreply.github.com>
2024-06-25 13:41:30 +0300

    Seperated physics to header and cpp file

--- 8BitBounce/Physics.h
+++ 8BitBounce/Physics.h
@@ -4,72 +4,32 @@
 #define SECOND_TO_MILISECOND 1000
 #define VELOCITY_THRESHOLD 10
 
-
 class Physics
 {
 private:
-
-		int GetTaskbarHeight()
-		{
-			APPBARDATA abd;
-			memset(&abd, 0, sizeof(abd));
-			abd.cbSize = sizeof(abd);
-
-			if (SHAppBarMessage(ABM_GETTASKBARPOS, &abd))
-			{
-				return abd.rc.bottom - abd.rc.top;
-			}
-			else
-			{
-				// If the function fails, return a default value (e.g., 0)
-				return 0;
-			}
-		}
+    int GetTaskbarHeight();
 
 protected:
+    virtual void UpdateSize();
 
-		virtual void UpdateSize()
-		{
-		}
+public:
+    HWND hWnd;
+    int screenWidth = GetSystemMetrics(SM_CXSCREEN);
+    int screenHeight = GetSystemMetrics(SM_CYSCREEN);
+    int taskbarHeight = GetTaskbarHeight();
+    int centerW = screenWidth / 2;
+    int centerH = screenHeight / 2;
 
-	public:
+    int width;
+    int height;
+    float deltaTime = 0;
+    DWORD lastTime;
+    DWORD currentTime;
 
-		HWND hWnd;
-		int screenWidth = GetSystemMetrics(SM_CXSCREEN);
-		int screenHeight = GetSystemMetrics(SM_CYSCREEN);
-		int taskbarHeight = GetTaskbarHeight();
-		int centerW = screenWidth / 2;
-		int centerH = screenHeight / 2;
+    Physics(HWND windowHandle);
 
-		int width;
-		int height;
-		float deltaTime = 0;
-		DWORD lastTime;
-		DWORD currentTime;
+    HWND GetWindowHandle();
+    void SetWindowHandle(HWND handle);
 
-		Physics(HWND windowHandle)
-		{
-			hWnd = windowHandle;
-			/*
-			if (!IsWindow(hWnd))
-			{
-				MessageBox(hWnd, L"a", L"", MB_OK);
-			}*/
-			UpdateSize();
-		}
-
-		HWND GetWindowHandle()
-		{
-			return this->hWnd;
-		}
-
-		void SetWindowHandle(HWND handle)
-		{
-			this->hWnd = handle;
-			UpdateSize();
-		}
-
-		void virtual RunPhysics()
-		{
-		}
+    virtual void RunPhysics();
 };
