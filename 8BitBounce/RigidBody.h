dcc34e0bb3fe4a9a8ac318813e4041261ee44a71
Snawyyy <76952482+Snawyyy@users.noreply.github.com>
2024-09-13 22:00:42 +0300

    Added ApplyAirResistance declaration and variables

--- 8BitBounce/RigidBody.h
+++ 8BitBounce/RigidBody.h
@@ -3,6 +3,8 @@
 #include <windows.h>
 #include "WindowManager.h"
 
+#define PI 3.14159265358979323846f
+
 struct Vector2
 {
     float x;
@@ -32,13 +34,22 @@ private:
     float friction = 0.4f;
     float staticFriction = 0.5f;
     float dampingFactor = 0.9f; // damping factor for energy loss on bounce
+
     const double G = 6.67430e-11;  // gravitational constant in m^3 kg^-1 s^-2
     const double EARTH_MASS = 5.9722e24;  // mass of the Earth in kg
     const double EARTH_RADIUS = 6.371e6;  // radius of the Earth in meters
+    float crossSectionalArea = PI * body.radius * body.radius;
+
+    const float airDensity = 1.225f; // kg/m^3 at sea level
+    float dragCoefficient = 0.47f;   // For a sphere; adjust based on shape
+
+    const float gravityAcceleration = 9.81f; // Acceleration due to gravity (m/s^2)
+    const float frictionCoefficient = 0.9;  // Coefficient of kinetic friction (adjust as needed)
 
     void CalculateTime();
     void UpdatePosition();
     void ApplyWorldGravity();
+    void ApplyAirResistance();
     void ApplyFriction();
     void BorderCollisions();
     void Draggable();
